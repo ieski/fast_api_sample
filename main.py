d9be1b56a2af24f5a48246cd3df5aae5cdbcc03c
ismail eski <ismaileski@gmail.com>
2022-09-05 13:35:13 +0300

    [ADD] ignore

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from typing import Union, List
 from uuid import UUID, uuid4
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Path, Query, HTTPException
 
 from models import User, Gender, Role, UserUpdateRequest
 
 
-app = FastAPI()
+app = FastAPI(
+    title="Api Gateway",
+    description="",
+    version="0.0.1",
+    terms_of_service="https://github.com/ieski",
+    contact={
+        "name": "ismail eski",
+        "url": "https://github.com/ieski",
+        "email": "ismaileski@gmail.com",
+    },
+    license_info={
+        "name": "AGPL-3.0-only",
+        "url": "https://www.gnu.org/licenses/agpl-3.0.tr.html",
+    },
+)
 
 
 db: List[User] = [
@@ -36,7 +50,9 @@ async def register_user(user: User):
 
 
 @app.delete("/api/v1/users/{user_id}")
-async def delete(user_id: UUID):
+async def delete(
+    user_id: UUID = Path(..., description="The ID of the User you want to delete.")
+):
     for user in db:
         if user.id == user_id:
             db.remove(user)
@@ -47,7 +63,10 @@ async def delete(user_id: UUID):
 
 
 @app.put("/api/v1/users/{user_id}")
-async def update_user(user_update: UserUpdateRequest, user_id: UUID):
+async def update_user(
+    user_update: UserUpdateRequest,
+    user_id: UUID = Path(..., description="The ID of the User you want to update."),
+):
     for user in db:
         if user.id == user_id:
             if user_update.first_name is not None:
